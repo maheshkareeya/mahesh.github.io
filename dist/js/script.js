8cda737b36268632c9b9bf1391ce3dabd3113e00
maheshkareeya <maheshkareeya@gmail.com>
2019-06-22 16:36:36 +0530

    ok

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator){
     window.addEventListener('load',()=>{
          navigator.serviceWorker
-        .register('../sw.js')
+        .register('../sw_site.js')
         .then((reg)=>console.log('service worker'))
         .catch(err=>console.log(err))
     })
