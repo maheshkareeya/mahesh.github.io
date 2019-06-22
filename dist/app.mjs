4dcb66a09ace3c49ad6a501f44412075e94ae284
maheshkareeya <maheshkareeya@gmail.com>
2019-06-22 23:05:20 +0530

    ok

--- dist/app.mjs
+++
@@ -1,7 +0,0 @@
-import { } from './web-elements/mk-head.mjs';
-import { } from './web-elements/mk-navbar.mjs';
-import { } from './web-elements/mk-table.mjs';
-import { } from './web-elements/mk-model-form.mjs';
-import { } from './web-elements/mk-sidebar.mjs';
-
-
